47fff7be22dc3dcf998e68d1ca7c4a1ce9abffa9
Hoo Man <hoomanfor@gmail.com>
2019-09-14 17:20:01 -0400

    added start button and timer

---
+++ assets/javascript/app.js
@@ -0,0 +1,27 @@
+ 
+var timer = 5;
+var intervalId;
+
+function start() {
+    $("#timer").text(timer);
+    intervalId = setInterval(decrement, 1000);
+}
+
+function decrement() {
+    timer--;
+    $("#timer").text(timer);
+    if (timer == 0) {
+        stop();
+    }
+}
+
+function stop() {
+    clearInterval(intervalId);
+}
+
+$("#start").click(function(event) {
+    $(this).css("display", "none");
+    $("#questions").css("display", "block");
+    start()
+});
+
